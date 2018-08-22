0c48c11fe49228f65a10b66696c37b2d796e05a0
DaisySAM <42440899+DaisySAM@users.noreply.github.com>
2018-08-22 18:58:15 +0300

    Create AIPatrol.cpp

---
+++ AIPatrol.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "StealthGameAI.h"
+#include "AIPatrol.h"
+#include "AIPatrolController.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "Perception/PawnSensingComponent.h"
+
+// Sets default values
+AAIPatrol::AAIPatrol()
+{
+ 	/*Initialize sensess*/
+	PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("PawnSensingComp"));
+	PawnSensingComp->SetPeripheralVisionAngle(90.f);
+}
+
+// Called when the game starts or when spawned
+void AAIPatrol::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (PawnSensingComp) {
+		PawnSensingComp->OnSeePawn.AddDynamic(this, &AAIPatrol::OnPlayerCaught);
+	}
+}
+
+// Called to bind functionality to input
+void AAIPatrol::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+}
+
+void AAIPatrol::OnPlayerCaught(APawn * Pawn)
+{
+	/*Get a reference to the player controller*/
+	AAIPatrolController* AIController = Cast<AAIPatrolController>(GetController());
+
+	if (AIController) {
+		GEngine->AddOnScreenDebugMessage(-1, 0.2, FColor::Black, TEXT("За вами гонятся!"));
+		AIController->SetPlayerCaught(Pawn);
+	}
+}
+
