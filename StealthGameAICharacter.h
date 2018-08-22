ec65cfdcf0384ab44189cd81db62dd3facec8512
DaisySAM <42440899+DaisySAM@users.noreply.github.com>
2018-08-22 19:10:30 +0300

    Create StealthGameAICharacter.h

---
+++ StealthGameAICharacter.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "GameFramework/Character.h"
+#include "PickupActor.h"
+#include "StealthGameAICharacter.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FUpdateInventoryDelegate, const TArray<APickupActor*>&, InventoryItems);
+
+UCLASS(config=Game)
+class AStealthGameAICharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+public:
+	AStealthGameAICharacter();
+
+	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	float BaseTurnRate;
+
+	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	float BaseLookUpRate;
+
+protected:
+
+	/** Called for forwards/backward input */
+	void MoveForward(float Value);
+
+	/** Called for side to side input */
+	void MoveRight(float Value);
+
+	/** Handler for when a touch input begins. */
+	void TouchStarted(ETouchIndex::Type FingerIndex, FVector Location);
+
+	/** Handler for when a touch input stops. */
+	void TouchStopped(ETouchIndex::Type FingerIndex, FVector Location);
+
+private:
+	TArray<APickupActor*> _inventory;
+
+public:
+	void AddToInventory(APickupActor* actor);
+
+	UFUNCTION(BlueprintCallable, Category="MyFunctions")
+	void UpdateInventory();
+
+	UPROPERTY(BlueprintAssignable, Category = "Pickup")
+	FUpdateInventoryDelegate OnUpdateInventory;
+
+protected:
+	// APawn interface
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+	// End of APawn interface
+
+public:
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+};
+
