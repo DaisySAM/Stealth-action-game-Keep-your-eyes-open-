99c97b813c5740d7dfdb9561d2befb6066ad5bfa
DaisySAM <42440899+DaisySAM@users.noreply.github.com>
2018-08-22 19:12:17 +0300

    Create StealthGameAIGameMode.h

---
+++ StealthGameAIGameMode.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "StealthGameAIGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AStealthGameAIGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AStealthGameAIGameMode();
+};
+
+
+
